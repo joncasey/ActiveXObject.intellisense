9de20307572afdee09b2431786f446522fba4a93
joncasey <joncasey@gmail.com>
2014-02-01 14:12:22 -0500

    Add a few more MSXML ids.

--- ActiveXObject.js
+++ ActiveXObject.js
@@ -1,7 +1,11 @@
 var ActiveXObject = ActiveXObject || function (id) {
   /// <param name="id">
   /// ADODB.Stream                   &#10;
+  /// Msxml2.DOMDocument             &#10;
   /// Msxml2.FreeThreadedDOMDocument &#10;
+  /// Msxml2.XMLHTTP                 &#10;
+  /// Msxml2.XMLSchemaCache          &#10;
+  /// Msxml2.XSLTemplate             &#10;
   /// Scripting.FileSystemObject     &#10;
   /// Shell.Application              &#10;
   /// WScript.Network                &#10;
